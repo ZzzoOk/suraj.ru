ca929078b48ccec6b7181cd6160018c2bfc91e54
Suraj Matkurbanov <zzzook@yandex.ru>
2021-07-12 18:56:37 +0300

    Minor fixes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { HashRouter as Router, Route } from 'react-router-dom';
 import './App.css'
 import Main from './main/Main'
 import Imt from './imt/Imt'
